69391b65f17a5f198726981d010e11157c8c34cc
Sébastian Le Merdy <sebastian.lemerdy@compte-nickel.fr>
2018-10-29 11:03:45 +0100

    Fix eventHandlers

--- app/eventHandlers/EventHandler.scala
+++ app/eventHandlers/EventHandler.scala
@@ -1,6 +1,7 @@
 package eventHandlers
 
 import event.{BoardEvent, Event, TicketEvent}
+import states.Board.{Archived, Running}
 import states.{Board, Ticket}
 
 trait EventHandler[State, E <: Event] {
@@ -9,13 +10,21 @@ trait EventHandler[State, E <: Event] {
 
 object BoardEventHandler extends EventHandler[states.Board, event.BoardEvent] {
   override def handle(e: BoardEvent)(state: Option[Board]): Option[Board] = {
-    ???
+    e match {
+      case BoardEvent.Created(_, title, description) => Some(Board(title, description, Running))
+      case BoardEvent.StatusChanged(Archived) => state.map(s => Board(s.title, s.description, Archived))
+      case _ => state
+    }
   }
 }
 
 object TicketEventHandler extends EventHandler[states.Ticket, event.TicketEvent] {
 
   override def handle(e: TicketEvent)(state: Option[states.Ticket]): Option[Ticket] = {
-    ???
+    e match {
+      case TicketEvent.Created(_, boardId, title, description, status) => Some(Ticket(status, boardId, title, description))
+      case TicketEvent.StatusChanged(boardId, status) => state.map(s => Ticket(status, boardId, s.title, s.description))
+      case _ => state
+    }
   }
 }
