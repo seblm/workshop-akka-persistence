f39bd7da97b9d8e197db680e75f8be8ac308b1c8
Sébastian Le Merdy <sebastian.lemerdy@compte-nickel.fr>
2018-10-29 13:58:24 +0100

    wip serialization

--- app/serialization/EventJsonEncoder.scala
+++ app/serialization/EventJsonEncoder.scala
@@ -6,7 +6,16 @@ import play.api.libs.json.{JsObject, Json}
 
 class EventJsonEncoder extends akka.persistence.pg.event.JsonEncoder {
 
-  override def toJson: PartialFunction[Any, JsonString] = ???
+  override def toJson: PartialFunction[Any, JsonString] = {
+    case e: BoardEvent =>
+      import event.BoardEvent._
+      JsonString(Json.toJson(e).toString())
+  }
 
-  override def fromJson: PartialFunction[(JsonString, Class[_]), AnyRef] = ???
+  override def fromJson: PartialFunction[(JsonString, Class[_]), AnyRef] = {
+    case (jsonEvent, classOf[BoardEvent]) =>
+      import event.BoardEvent._
+      Json.fromJson(JsObj ect())
+  }
+
 }
